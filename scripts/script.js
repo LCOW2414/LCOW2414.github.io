d952c5f07ebe195b75996c44f5cce269ffca3437
Shweta <shweta@terminal2online.com>
2024-04-13 03:05:41 -0400

    Implemented remaining points.

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+var video = document.
+    getElementById("example");
+var videoSource = document.
+    getElementById("vid-src");
+var descriptionSource = document.
+    getElementById("despsrc");
+
+function hamburger() {
+    var menu = document.
+        getElementById("menu-links");
+    var logo =
+        document.getElementById("ffc-logo");
+    if (menu.style.display === "block" &&
+        logo?.style.display === "none") {
+        menu.style.display =
+            "none";
+        logo.style.display = "block";
+    }
+    else {
+        menu.style.display = "block";
+        logo.style.display = "none";
+    }
+}
